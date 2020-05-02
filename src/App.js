d07fa77bf547be9292bc8e188661edccf841a627
Kamil Biedrzycki <biedrzycki.kamil@gmail.com>
2020-05-02 21:39:43 +0200

    Fix linter, it has to happen after pull request is opened

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         </p>
         <a
           className="App-link"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
